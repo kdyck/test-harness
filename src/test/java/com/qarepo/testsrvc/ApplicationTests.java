fefc4aecd76e2922d4282068dff8a0eaf60be289
Kaci Dyck <kdyck00@gmail.com>
2021-07-10 12:59:49 -0400

    TH-1 init commit

---
+++ src/test/java/com/qarepo/testsrvc/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.qarepo.testsrvc;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
